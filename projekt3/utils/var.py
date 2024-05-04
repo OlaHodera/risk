2af60b9cab0948d054f28a5e4f9d2b4a99b0021f
mlubneuskaya <mlubnewskaja@gmail.com>
2024-05-04 22:58:46 +0200

    wykresy

--- projekt3/utils/var.py
+++ projekt3/utils/var.py
@@ -1,8 +1,15 @@
 import numpy as np
 
+from projekt3.utils.download import parse_json
+
+params = parse_json()
+lam = params['lambda']
+alpha = params['alpha']
+window = params['okno']
+
 
 def calculate_var(df, method, name):
-    df[f'var95_{name}'] = df.returns.rolling(window=125).agg(method)
+    df[f'var95_{name}'] = df.returns.rolling(window=window).agg(method)
     df[f'VaR95_{name}'] = df[f'var95_{name}'] * df.Close.shift(-1) + df.Close.shift(-1)
     return df
 
