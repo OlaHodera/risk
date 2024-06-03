a2e1eb031e089254b75b8461b98a2fecd4f50b3a
mlubneuskaya <mlubnewskaja@gmail.com>
2024-06-03 22:50:43 +0200

    var parametryczny

--- projekt4/var/multi_asset_var.py
+++ projekt4/var/multi_asset_var.py
@@ -1,10 +1,12 @@
-import scipy.stats as stats
 import numpy as np
+from scipy import stats
 
 
-def var(L, weights, alpha, days=1):
-    mean = L.mean()
-    cov = L.cov()
+def var(L, weights, alpha, days=1, mean=None, cov=None):
+    if mean is None and cov is None:
+        mean = L.mean()
+        cov = L.cov()
     portfolio_mean = np.dot(mean, weights)
     portfolio_variance = np.dot(weights ** 2, np.diag(cov) ** 2) + 2 * (weights * (weights * cov).T).sum().sum()
     return np.sqrt(portfolio_variance * days) * stats.norm.ppf(alpha) + portfolio_mean * days
+
